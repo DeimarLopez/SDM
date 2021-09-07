cab16860face4af5ba34dfd369c0310175df7b25
Deimar Lopez <deimarlopez115d@gmail.com>
2021-09-06 20:37:16 -0500

    b

--- model/conexion.php
+++ model/conexion.php
@@ -6,7 +6,7 @@ class Conexion{
 
         $_URL='mysql:host=localhost;dbname=sdm';
         $_USER = 'root';
-        $_PASSWORD = 'NANCY';
+        $_PASSWORD = '';
 
         try{
             $con= new PDO($_URL,$_USER,$_PASSWORD);
